1e1889e50c16134c187e62afb75f0f6d00d42ef6
johnloser <66107110+johnloser-lwi@users.noreply.github.com>
2025-06-30 21:29:23 -0700

    update activities copy

--- js/get_activities.js
+++ js/get_activities.js
@@ -5,20 +5,20 @@
         {
             date: "Thursday, July 21",
             img: "./assets/passes/festival.png",
-            title: "",
-            description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi ad reprehenderit iste velit ipsam libero non dolore. Vitae reiciendis ipsam ipsum aliquam culpa, ullam iste eaque excepturi dicta corporis nostrum.",
+            title: "Glow Paint Station",
+            description: "Unleash your inner artist with UV-reactive face and body paint. Whether you’re going full festival glam or just adding a splash of color, our neon station keeps you glowing all night long.",
         },
         {
             date: "Friday, July 22",
             img: "./assets/passes/festival.png",
-            title: "",
-            description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi ad reprehenderit iste velit ipsam libero non dolore. Vitae reiciendis ipsam ipsum aliquam culpa, ullam iste eaque excepturi dicta corporis nostrum.",
+            title: "DIY Craft Garden",
+            description: "Take a breather and get hands-on with creative DIY booths. Design your own kandi bracelets, customize tote bags, or leave your mark on our collaborative community mural.",
         },
         {
             date: "Saturday, July 23",
             img: "./assets/passes/festival.png",
-            title: "",
-            description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi ad reprehenderit iste velit ipsam libero non dolore. Vitae reiciendis ipsam ipsum aliquam culpa, ullam iste eaque excepturi dicta corporis nostrum.",
+            title: "Lawn Games & Chill Zone",
+            description: "Need a break from dancing? Head over to the grass for jumbo Jenga, hula hoops, and chill hammocks. It’s the perfect spot to reset, connect, and vibe out under the sky.",
         },
     ];
 
@@ -30,7 +30,10 @@
                 <img src="${activity.img}" alt="${activity.title}"></img>
                 <div class="activities-card-info">
                     <div>
-                        <h3>${activity.date}</h3>
+                        <div>
+                            <h3>${activity.title}</h3>
+                            <h4>${activity.date}</h4>
+                        </div>
                         <p>${activity.description}</p>
                     </div>
                     <button class="activities-expand-button">Expand +</button>
